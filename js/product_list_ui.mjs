41650ca678702a082feb64f94065d24b3d01bd28
Saverio Trioni <saverio.t@toptal.com>
2024-05-04 14:13:11 +0200

    Use handlebars for resolving template

--- js/product_list_ui.mjs
+++ js/product_list_ui.mjs
@@ -15,25 +15,12 @@ function displayProducts(products) {
     console.log(template.content.firstElementChild)
     let item = template.content.firstElementChild.cloneNode(true)
 
-
-    // selecccion de todos los atributos data-field
-    for (let element of item.querySelectorAll("[data-field]")) {
-      let field = element.dataset.field
-      // dataset recoge los atributos data y como tenemos data-field
-      // dentro de field encontraremos cada elemento
-      // en cada repeticion tratamos un elemento "dd"
-
-      let value = product[field] // acceder a un objeto
-      // equivalente a: product.brand si field fuera "brand"
-
-
-      element.innerText = value //innerText pondrá el texto dentro de los tags <dd>
-
-    }
+    let hbTemplate = Handlebars.compile(item.innerHTML);
+    let output = hbTemplate(product)
+    item.innerHTML = hbTemplate
 
     target.appendChild(item)
     console.log(item)
-    item.setAttribute("id", product.id)
   }
 }
 
